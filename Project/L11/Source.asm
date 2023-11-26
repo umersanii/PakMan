4434ffe17403a3ca55c7e33c64bd0a3971b3b77b
umersanii <iamumersani@gmail.com>
2023-11-26 23:35:50 +0500

    0.1
    :arrow_right:  Basic Square Wall
    :arrow_right:  Collision Logic (50%)

--- Project/L11/Source.asm
+++ Project/L11/Source.asm
@@ -1,142 +1,176 @@
-
 INCLUDE Irvine32.inc
 
 .data
+    ground BYTE "------------------------------------------------------------------------------------------------------------------------",0
+    strScore BYTE "Your score is: ",0
+vertical BYTE "|                                                                                                          |",0
 
-ground BYTE "------------------------------------------------------------------------------------------------------------------------",0
+    score BYTE 0
 
-strScore BYTE "Your score is: ",0
-score BYTE 0
+    xPos BYTE 20
+    yPos BYTE 20
 
-xPos BYTE 20
-yPos BYTE 20
+    MIN_X equ 0
+    MIN_Y equ 0
+    MAX_X equ 40 ; Assuming a console width of 80 characters
+    MAX_Y equ 27
 
-xCoinPos BYTE ?
-yCoinPos BYTE ?
+    xCoinPos BYTE ?
+    yCoinPos BYTE ?
 
-inputChar BYTE ?
+    inputChar BYTE ?
 
 .code
+DrawBoundry PROC
+mov dl, al
+mov dh, ah
+call Gotoxy
+mov edx, OFFSET vertical
+call WriteString
+ret
+DrawBoundry ENDP
 main PROC
-	mov eax,Black +(white*16)
-	call SetTextColor
-	call clrscr
-	; draw ground at (0,29):
-	mov dl,0
-	mov dh,29
-	call Gotoxy
-	mov edx,OFFSET ground
-	call WriteString
+    mov eax,Black +(white*16)
+    call SetTextColor
+    call clrscr
+    mov dl, 0
+    mov dh, 0
+    call Gotoxy
+    mov edx, OFFSET ground
+    call WriteString
+    mov al, 0
+    mov ecx, 29
+LOOP1:
+    inc ah
+    mov bl, vertical
+    call DrawBoundry
+    loop LOOP1
+    mov dl, 0
+    mov dh, 29
+    call Gotoxy
+    mov edx, OFFSET ground
+    call WriteString
 
-	call DrawPlayer
 
-	call CreateRandomCoin
-	call DrawCoin
+    call DrawPlayer
+    call CreateRandomCoin
+    call DrawCoin
 
-	call Randomize
+    call Randomize
 
-	gameLoop:
+    gameLoop:
+        ; getting points:
+        mov bl,xPos
+        cmp bl,xCoinPos
+        jne notCollecting
+        mov bl,yPos
+        cmp bl,yCoinPos
+        jne notCollecting
+        ; player is intersecting coin:
+        inc score
+        call CreateRandomCoin
+        call DrawCoin
+        notCollecting:
 
-		; getting points:
-		mov bl,xPos
-		cmp bl,xCoinPos
-		jne notCollecting
-		mov bl,yPos
-		cmp bl,yCoinPos
-		jne notCollecting
-		; player is intersecting coin:
-		inc score
-		call CreateRandomCoin
-		call DrawCoin
-		notCollecting:
+        mov eax,white+(black * 16)
+        call SetTextColor
 
-		mov eax,white+(black * 16)
-		call SetTextColor
+        ; draw score:
+        mov dl,0
+        mov dh,0
+        call Gotoxy
+        mov edx,OFFSET strScore
+        call WriteString
+        mov al,score
+        call WriteInt
 
-		; draw score:
-		mov dl,0
-		mov dh,0
-		call Gotoxy
-		mov edx,OFFSET strScore
-		call WriteString
-		mov al,score
-		call WriteInt
+        ; gravity logic:
+        gravity:
+        cmp yPos,MAX_Y
+        jg onGround
+        ; make player fall:
+        call UpdatePlayer
+        inc yPos
+        call DrawPlayer
+        mov eax,80
+        call Delay
+        jmp gravity
+        onGround:
 
-		; gravity logic:
-		gravity:
-		cmp yPos,27
-		jg onGround
-		; make player fall:
-		call UpdatePlayer
-		inc yPos
-		call DrawPlayer
-		mov eax,80
-		call Delay
-		jmp gravity
-		onGround:
+        call ReadChar
+        mov inputChar, al
 
-		; get user key input:
-		call ReadChar
-		mov inputChar,al
+        ; exit game if user types 'x':
+        cmp inputChar, "x"
+        je exitGame
 
-		; exit game if user types 'x':
-		cmp inputChar,"x"
-		je exitGame
+        ; Handle player movement
+        cmp inputChar, "w"
+        je moveUp
+        cmp inputChar, "s"
+        je moveDown
+        cmp inputChar, "a"
+        je moveLeft
+        cmp inputChar, "d"
+        je moveRight
 
-		cmp inputChar,"w"
-		je moveUp
+        moveUp:
+            ; Check boundary before moving up
+            cmp yPos, MIN_Y
+            jle skipMoveUp
+            call UpdatePlayer
+            dec yPos
+            call DrawPlayer
+            skipMoveUp:
+            jmp gameLoop
 
-		cmp inputChar,"s"
-		je moveDown
+        moveDown:
+            ; Check boundary before moving down
+            cmp yPos, MAX_Y
+            jge skipMoveDown
+            call UpdatePlayer
+            inc yPos
+            call DrawPlayer
+            skipMoveDown:
+            jmp gameLoop
 
-		cmp inputChar,"a"
-		je moveLeft
+        moveLeft:
+            ; Check boundary before moving left
+            cmp xPos, MIN_X
+            jle skipMoveLeft
+            call UpdatePlayer
+            dec xPos
+            call DrawPlayer
+            skipMoveLeft:
+            jmp gameLoop
 
-		cmp inputChar,"d"
-		je moveRight
+        moveRight:
+            ; Check boundary before moving right
+            cmp xPos, MAX_X
+            jge skipMoveRight
+            call UpdatePlayer
+            inc xPos
+            call DrawPlayer
+            skipMoveRight:
+            jmp gameLoop
 
-		moveUp:
-		; allow player to jump:
-		mov ecx,1
-		jumpLoop:
-			call UpdatePlayer
-			dec yPos
-			call DrawPlayer
-			mov eax,70
-			call Delay
-		loop jumpLoop
-		jmp gameLoop
+    jmp gameLoop
 
-		moveDown:
-		call UpdatePlayer
-		inc yPos
-		call DrawPlayer
-		jmp gameLoop
-
-		moveLeft:
-		call UpdatePlayer
-		dec xPos
-		call DrawPlayer
-		jmp gameLoop
-
-		moveRight:
-		call UpdatePlayer
-		inc xPos
-		call DrawPlayer
-		jmp gameLoop
-
-	jmp gameLoop
-
-	exitGame:
-	exit
+    exitGame:
+        exit
 main ENDP
+
+; ... (the rest of your code remains unchanged)
+
+
+
 
 DrawPlayer PROC
 	; draw player at (xPos,yPos):
 	mov dl,xPos
 	mov dh,yPos
 	call Gotoxy
-	mov al,"X"
+	mov al,"O"
 	call WriteChar
 	ret
 DrawPlayer ENDP
